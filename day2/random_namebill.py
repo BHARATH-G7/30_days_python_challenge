8ff9610c427e750e0aaaf6ee942fd4f074b7ab20
BHARATH-G7 <bharathrajg22@gmail.com>
2022-12-10 10:35:19 +0530

    completed random name bill

---
+++ day2/random_namebill.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+import random
+name=["Sheik","Mahe","Bharath","Mohammed","Mohideen","Raj","Tamil","Vijay","Ajith"]
+a=random.choice(name)
+print(f"\n{a} has to pay the bill..")
